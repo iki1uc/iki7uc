19d90081dd8fdcdd08bafb1b9859fe7823a1f21f
iki1uc <entertrainer@web.de>
2026-06-02 00:15:08 +0200

    Update PASSAGE_ENGINE.js

--- passage/PASSAGE_ENGINE.js
+++ passage/PASSAGE_ENGINE.js
@@ -1,1 +1,12 @@
+// PASSAGE_ENGINE.js
+export function PASSAGE_ENGINE(molekuel) {
+    return [
+        molekuel.passage.in2,
+        molekuel.passage.me2,
+        molekuel.passage.in4,
+        molekuel.passage.run4,
+        molekuel.passage.me4,
+        molekuel.passage.u4
+    ];
+}
 
