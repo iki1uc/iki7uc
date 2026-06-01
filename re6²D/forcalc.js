973e9c4b2e28242804ca33c5e8a9cd9c1269eeab
iki1uc <entertrainer@web.de>
2026-06-01 22:03:46 +0200

    Update forcalc.js

--- re6²D/forcalc.js
+++ re6²D/forcalc.js
@@ -1,1 +1,10 @@
+export function forcalc(state){
+    return {
+        axis: "3x2",
+        direction: "forward",
+        value: state.value,
+        raw: state.raw ?? null,
+        mult: state.mult ?? 1
+    };
+}
 
