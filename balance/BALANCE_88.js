25992e53c7ae27aeb2a01c0f84294ebf7ab3017b
iki1uc <entertrainer@web.de>
2026-06-02 00:17:08 +0200

    Update BALANCE_88.js

--- balance/BALANCE_88.js
+++ balance/BALANCE_88.js
@@ -1,1 +1,12 @@
+// BALANCE_88.js
+export function BALANCE_88(molekuel) {
+    const diff = molekuel.acht88.links - molekuel.acht88.rechts;
+
+    return {
+        links: molekuel.acht88.links,
+        rechts: molekuel.acht88.rechts,
+        drift: diff,
+        stabil: diff === 0
+    };
+}
 
