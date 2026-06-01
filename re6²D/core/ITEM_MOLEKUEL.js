5358c3b9cca8029d12733da1b9e2a3e10e77c9d9
iki1uc <entertrainer@web.de>
2026-06-01 23:58:07 +0200

    Update ITEM_MOLEKUEL.js

--- re6²D/core/ITEM_MOLEKUEL.js
+++ re6²D/core/ITEM_MOLEKUEL.js
@@ -1,1 +1,41 @@
+export const ITEM_MOLEKUEL = {
+
+    // 1) NAVI – virtuelle Achsen (Bewegungsprofile)
+    navi: {
+        ui: 0,
+        ie: 0,
+        ah: 0,
+        ha: 0,
+        äh: 0,
+        hä: 0
+    },
+
+    // 2) PASSAGE – Pipeline-Signatur (6 Stationen)
+    passage: {
+        in2: null,
+        me2: null,
+        in4: null,
+        run4: null,
+        me4: null,
+        u4: null
+    },
+
+    // 3) XX – Rohmuster (Scanner-Eingang)
+    xx: {
+        roh: "",
+        signatur: ""
+    },
+
+    // 4) 88 – Balance (Gegenanker)
+    acht88: {
+        links: 0,
+        rechts: 0
+    },
+
+    // 5) META – minimale Identität
+    meta: {
+        id: "ITEM-BASE",
+        version: "1.0"
+    }
+};
 
