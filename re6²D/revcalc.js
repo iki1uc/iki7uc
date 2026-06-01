0ea357a5227689bc96c6aaddca556dffff37c8ad
iki1uc <entertrainer@web.de>
2026-06-01 22:08:54 +0200

    Update revcalc.js

--- re6²D/revcalc.js
+++ re6²D/revcalc.js
@@ -1,1 +1,11 @@
+// revcalc.js
+export function revcalc(state){
+    return {
+        axis: "3x1",
+        direction: "backward",
+        value: state.value,
+        raw: state.raw ?? null,
+        mult: state.mult ?? 1
+    };
+}
 
