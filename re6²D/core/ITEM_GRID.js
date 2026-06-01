6b3ba8a15e611e8eaeab24ab70747fcc32b7026a
iki1uc <entertrainer@web.de>
2026-06-02 00:05:43 +0200

    Update ITEM_GRID.js

--- re6²D/core/ITEM_GRID.js
+++ re6²D/core/ITEM_GRID.js
@@ -1,1 +1,25 @@
+import { ITEM_MOLEKUEL } from "./ITEM_MOLEKUEL.js";
+
+// 4×4 Fläche – jedes Feld = ein Molekül mit Ort
+export const ITEM_GRID_4x4 = [
+  [ { ...ITEM_MOLEKUEL, pos: { x: 0, y: 0 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 1, y: 0 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 2, y: 0 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 3, y: 0 } } ],
+
+  [ { ...ITEM_MOLEKUEL, pos: { x: 0, y: 1 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 1, y: 1 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 2, y: 1 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 3, y: 1 } } ],
+
+  [ { ...ITEM_MOLEKUEL, pos: { x: 0, y: 2 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 1, y: 2 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 2, y: 2 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 3, y: 2 } } ],
+
+  [ { ...ITEM_MOLEKUEL, pos: { x: 0, y: 3 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 1, y: 3 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 2, y: 3 } },
+    { ...ITEM_MOLEKUEL, pos: { x: 3, y: 3 } } ]
+];
 
