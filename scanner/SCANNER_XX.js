b837c47bd02c0a802d3ca5aaba06443e35f2e619
iki1uc <entertrainer@web.de>
2026-06-02 00:28:57 +0200

    Delete scanner directory

--- scanner/SCANNER_XX.js
+++
@@ -1,9 +0,0 @@
-// SCANNER_XX.js
-export function SCANNER_XX(molekuel) {
-    return {
-        roh: molekuel.xx.roh,
-        signatur: molekuel.xx.signatur,
-        länge: molekuel.xx.signatur.length
-    };
-}
-
