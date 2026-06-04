8e85eeec43c614e2f1c047cbe70005a4f26ff4f4
iki1uc <entertrainer@web.de>
2026-06-04 08:11:44 +0200

    Update 6iygo.js

--- transwarp/tam/6iygo.js
+++ transwarp/tam/6iygo.js
@@ -1,1 +1,21 @@
+// /tam/6iygo.js
+// transwarp – TAM-Paket für 3×3
+
+export const paket = {
+    typ: "tam",
+    mode: "transwarp",
+    items: [
+        { item: "tw1", molekuel: "Transwarp Start",   ort: "r0c0" },
+        { item: "tw2", molekuel: "Sprungfeld",        ort: "r0c1" },
+        { item: "tw3", molekuel: "Ankunftsraum",      ort: "r0c2" },
+
+        { item: "tw4", molekuel: "Frequenz",          ort: "r1c0" },
+        { item: "tw5", molekuel: "Korridor",          ort: "r1c1" },
+        { item: "tw6", molekuel: "Stabilisator",      ort: "r1c2" },
+
+        { item: "tw7", molekuel: "Materialisierung",  ort: "r2c0" },
+        { item: "tw8", molekuel: "Vollbild",          ort: "r2c1" },
+        { item: "tw9", molekuel: "TAM-Platz",         ort: "r2c2" }
+    ]
+};
 
