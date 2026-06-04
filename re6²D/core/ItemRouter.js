fd50d5eaca70823b71f5b967f805821047ab1a8f
iki1uc <entertrainer@web.de>
2026-06-04 23:15:40 +0200

    Update ItemRouter.js

--- re6²D/core/ItemRouter.js
+++ re6²D/core/ItemRouter.js
@@ -1,1 +1,26 @@
+export function ITEM_ROUTER(m) {
+    const direction = m.navi?.direction ?? "neutral";
+    const value = m.impulse?.value ?? 0;
+    const mult = m.impulse?.mult ?? 1;
+
+    const xx = {
+        roh: m.xx?.roh ?? "",
+        signatur: m.xx?.signatur ?? "",
+        länge: (m.xx?.signatur ?? "").length
+    };
+
+    const balance = BALANCE_88(m);
+
+    const coreInput = { direction, value, mult };
+    const coreResult = processImpulse(coreInput);
+
+    return {
+        pos: m.pos,
+        meta: m.meta,
+        xx,
+        balance,
+        input: coreInput,
+        core: coreResult
+    };
+}
 
